33770592515678acebfb98372be4b0ab34fcf6a1
Noorul <noorulahsanpp@gmail.com>
2020-04-18 23:45:12 +0530

    profile picture pick from gallery

--- app/src/main/java/com/example/hostelapp/Home.java
+++ app/src/main/java/com/example/hostelapp/Home.java
@@ -1,26 +1,32 @@
 package com.example.hostelapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class Home extends AppCompatActivity {
 
-    private Button logoutBtn;
-    private Button NotificationBtn;
-    private Button AttendanceBtn;
-    private Button FeesBtn;
-    private Button MessoutBtn;
-    private Button SickBtn;
+    private Button logoutBtn, NotificationBtn, AttendanceBtn, FeesBtn, MessoutBtn, SickBtn;
     private FirebaseAuth auth;
-//    private TextView emailView;
+    private FirebaseFirestore firebaseFirestore;
+    private String userID;
+    private String userName;
+    private TextView userNameView;
+    private ImageView profilePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +34,33 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         auth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        userID = auth.getCurrentUser().getUid();
 
-//        emailView = (TextView)findViewById(R.id.emailView);
-
-//        FirebaseUser user = auth.getCurrentUser();
-
-//        emailView.setText(user.getEmail());
-
+        profilePicture = findViewById(R.id.imageView2);
+        userNameView = findViewById(R.id.textView5);
         logoutBtn = (Button)findViewById(R.id.button2);
         NotificationBtn = (Button)findViewById(R.id.button5);
         AttendanceBtn = (Button)findViewById(R.id.button6);
         FeesBtn = (Button)findViewById(R.id.button7);
         MessoutBtn = (Button)findViewById(R.id.button8);
         SickBtn = (Button)findViewById(R.id.button9);
+
+        DocumentReference documentReference = firebaseFirestore.collection("users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+
+                userNameView.setText("Welcome "+documentSnapshot.getString("Name"));
+            }
+        });
+
+        profilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, Profile.class));
+            }
+        });
 
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/hostelapp/Profile.java
+++ app/src/main/java/com/example/hostelapp/Profile.java
@@ -1,14 +1,83 @@
 package com.example.hostelapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class Profile extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private FirebaseFirestore firebaseFirestore;
+    private ImageView profilePicture;
+    private EditText name, email, phone;
+    private Button changePassword, editProfile;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        auth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        userID = auth.getCurrentUser().getUid();
+
+        profilePicture = findViewById(R.id.imageView);
+        name = findViewById(R.id.editText);
+        email = findViewById(R.id.editText3);
+        phone = findViewById(R.id.editText4);
+        changePassword = findViewById(R.id.button12);
+        editProfile = findViewById(R.id.button13);
+
+        DocumentReference documentReference = firebaseFirestore.collection("users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+
+                name.setText(documentSnapshot.getString("Name"));
+                email.setText(documentSnapshot.getString("Email"));
+                phone.setText(documentSnapshot.getString("Phone"));
+
+            }
+        });
+
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGallery, 1000 );
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1000)
+        {
+            if(resultCode == Activity.RESULT_OK)
+            {
+                Uri imageUri = data.getData();
+                profilePicture.setImageURI(imageUri);
+            }
+
+        }
     }
 }
