5306dea5c1a243d6c405a7178cf263044ac40593
Noorul <noorulahsanpp@gmail.com>
2021-01-18 22:20:38 +0530

    email us feature added

--- app/src/main/java/com/example/hostelapp/Contactus.java
+++ app/src/main/java/com/example/hostelapp/Contactus.java
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NavUtils;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Contactus extends AppCompatActivity {
 TextView faqTV;
+Button next;
+EditText problemEt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,29 @@ TextView faqTV;
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Contact Us");
         faqTV = findViewById(R.id.faqTv);
+        problemEt = findViewById(R.id.description);
+        next = findViewById(R.id.button);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = problemEt.getText().toString();
+                String[] TO = {"noorulahsanpp@gmail.com"};
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+                emailIntent.setData(Uri.parse("mailto:"));
+                emailIntent.setType("text/plain");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Hostelapp Complaint");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, message+"");
+
+                try {
+                    startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+                    finish();
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(Contactus.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         faqTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
