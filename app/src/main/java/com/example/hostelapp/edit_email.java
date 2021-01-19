01a463e589cd12fe4ca49c9520b49d73be0b2ae2
Noorul <noorulahsanpp@gmail.com>
2021-01-19 14:10:52 +0530

    change email added

--- app/src/main/java/com/example/hostelapp/edit_email.java
+++ app/src/main/java/com/example/hostelapp/edit_email.java
@@ -1,13 +1,25 @@
 package com.example.hostelapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class edit_email extends AppCompatActivity {
 
@@ -19,8 +31,42 @@ public class edit_email extends AppCompatActivity {
         setContentView(R.layout.activity_edit_email);
         getSupportActionBar().setTitle("Edit Email");
         popup();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         editemailTV = findViewById(R.id.editEmail);
         EditBtn = findViewById(R.id.EditBtn);
+
+        EditBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newMail = editemailTV.getText().toString().trim();
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // Get auth credentials from the user for re-authentication
+                AuthCredential credential = EmailAuthProvider
+                        .getCredential("user@example.com", "sample"); // Current Login Credentials \\
+                // Prompt the user to re-provide their sign-in credentials
+                user.reauthenticate(credential)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                //Now change your email address \\
+                                //----------------Code for Changing Email Address----------\\
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                user.updateEmail("noorulahsanpp@gmail.com")
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(getApplicationContext(), "Email added", Toast.LENGTH_LONG).show();
+                                                }
+                                            }
+                                        });
+                                //----------------------------------------------------------\\
+                            }
+                        });
+
+            }
+        });
+
     }
     private void popup() {
         DisplayMetrics dm = new DisplayMetrics();
